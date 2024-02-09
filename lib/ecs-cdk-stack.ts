56e9edd34e9d441d9ea03be74fe6a10e9e0b4e3e
tamashiro-syuta <syuta5825@gmail.com>
2024-02-09 19:33:21 +0900

    ✨ ECS Patterns でECSonFargateをデプロイ

--- lib/ecs-cdk-stack.ts
+++ lib/ecs-cdk-stack.ts
@@ -1,16 +1,26 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecsp from 'aws-cdk-lib/aws-ecs-patterns';
 
+// NOTE: 1スタックにつき1デプロイ
 export class EcsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'EcsCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // NOTE: Amazon ECS Patterns(ecsp) => Amazon ECS の使用時に高レベルの抽象化を提供
+    new ecsp.ApplicationLoadBalancedFargateService(this, 'MyWebServer', {
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
+      },
+      publicLoadBalancer: true
+    });
   }
 }
+
+// NOTE: 作成されるリソースは以下
+// NOTE: - Amazon ECS クラスター
+// NOTE: - Amazon VPC と Amazon EC2 インスタンス
+// NOTE: - Auto Scaling グループ
+// NOTE: - Application Load Balancer
+// NOTE: - IAM ロールとポリシー
