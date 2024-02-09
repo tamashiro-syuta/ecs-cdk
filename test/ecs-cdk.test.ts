dc1c5c9d26eebf60af434130f60321723336d8be
tamashiro-syuta <syuta5825@gmail.com>
2024-02-09 18:55:01 +0900

    🎉 cdk init --language typescript

---
+++ test/ecs-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EcsCdk from '../lib/ecs-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ecs-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EcsCdk.EcsCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
